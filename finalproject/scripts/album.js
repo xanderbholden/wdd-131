c6c78255dd8895addc6c14c754093b0dd6624b05
xanderbholden <xanderbholden@gmail.com>
2024-06-01 23:01:38 -0600

    Alot of Changes to the CSS, Html, and js

--- finalproject/scripts/album.js
+++ finalproject/scripts/album.js
@@ -28,83 +28,83 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const temples = [
     {
-        templeName: "Aba Nigeria",
-        location: "Aba, Nigeria",
-        dedicated: "2005, August, 7",
+        templeName: "Hill AirForce Base Musemum",
+        location: "Clearfield, Utah",
+        dedicated: "2016, July, 7th",
         area: 11500,
-        Picture: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+        imageUrl: "images/F-15-eagle(2).jpg"
     },
     {
-        templeName: "Manti Utah",
-        location: "Manti, Utah, United States",
-        dedicated: "1888, May, 21",
+        templeName: "Hill AirForce Base Musemum",
+        location: "Clearfield, Utah",
+        dedicated: "2016, July, 7th",
         area: 74792,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        imageUrl: "images/f15-eagle2(1).jpg"
     },
     {
-        templeName: "Payson Utah",
-        location: "Payson, Utah, United States",
-        dedicated: "2015, June, 7",
+        templeName: "Shark Bate Ooo HA HA ",
+        location: "Living Planet Aquarium",
+        dedicated: "2018, June, 20",
         area: 96630,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+        imageUrl: "images/shark.jpg"
     },
     {
-        templeName: "Yigo Guam",
-        location: "Yigo, Guam",
-        dedicated: "2020, May, 2",
+        templeName: "4th of July and Sparklers",
+        location: "Clearfield, Utah",
+        dedicated: "2019, July, 4th",
         area: 6861,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+        imageUrl: "images/4thofjuly.jpg"
     },
     {
-        templeName: "Washington D.C.",
-        location: "Kensington, Maryland, United States",
-        dedicated: "1974, November, 19",
+        templeName: "2nd Child incoming!",
+        location: "Spanish Fork, Utah",
+        dedicated: "2020, Sep, 16th",
         area: 156558,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+        imageUrl: "images/itsaboy2.jpg"
     },
     {
-        templeName: "Lima Perú",
-        location: "Lima, Perú",
-        dedicated: "1986, January, 10",
+        templeName: "Disneyland Vacation (Rockets)",
+        location: "Anehiem, California",
+        dedicated: "2021, November, 16th",
         area: 9600,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+        imageUrl: "images/rocketdisney.jpg"
     },
     {
-        templeName: "Mexico City Mexico",
-        location: "Mexico City, Mexico",
-        dedicated: "1983, December, 2",
+        templeName: "Disneyland's Merriest Nights!",
+        location: "Anehiem, California",
+        dedicated: "2021, November, 16th",
         area: 116642,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+        imageUrl: "images/christmasdisney2.jpg"
     },
     {
-        templeName: "Nauvoo Illinois",
-        location: "Nauvoo, Illinois, United States",
-        dedicated: "1846, May, 1",
+        templeName: "Merriest Night's party",
+        location: "Anehiem, California",
+        dedicated: "2021, November, 16th",
         area: 54000,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/nauvoo-illinois/800x500/nauvoo-temple-756499-wallpaper.jpg"
+        imageUrl: "images/Disneyposterchristmas.jpg"
     },
     {
-        templeName: "Rome Italy",
-        location: "Rome, Italy",
-        dedicated: "2019, March, 10",
+        templeName: "Got to see Princess Jasmine!",
+        location: "Anehiem, California",
+        dedicated: "2023, June 3rd",
         area: 40000,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rome-italy/2019/800x500/6-Rome-Temple-2160338.jpg"
+        imageUrl: "images/jasmine.jpg"
     },
     {
-        templeName: "Rexburg Idaho",
-        location: "Rexburg, Idaho, United States",
-        dedicated: "2008, February, 10",
+        templeName: "Sophie hanging with Goofy!",
+        location: "Anehiem, California",
+        dedicated: "2023, June 4rth",
         area: 57000,
         imageUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjF021utolG7yeE4RN2UhMu03u52HgR39JigFofLndqA&s",
+            "images/sophieGoofy.jpg",
         alt: "Rexburg Idaho Temple",
     },
     {
-        templeName: "Kyiv Ukraine",
-        location: "Kyiv, Ukraine",
-        dedicated: "2010, August, 29",
+        templeName: "A Friend's Wedding",
+        location: "Pleasant Grove, Utah",
+        dedicated: "2023 August 23rd",
         area: 37500,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/kyiv-ukraine/800x1280/kyiv-ukraine-temple-lds-807707-wallpaper.jpg"
+        imageUrl: "images/family-wedding.jpg"
     }
 ];
 
@@ -147,8 +147,8 @@ function displayTempleCards(option) {
         templeCard.innerHTML = `
             <h3>${temple.templeName}</h3>
             <p>Location: ${temple.location}</p>
-            <p>Dedicated: ${temple.dedicated}</p>
-            <p>Area: ${temple.area} sq ft</p>
+            <p>Date: ${temple.dedicated}</p>
+           
             <p class="space"> </p>
             <img src="${temple.imageUrl}" alt="${temple.templeName}" loading="lazy">
         `;
